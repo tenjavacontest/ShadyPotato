1ddfea7accb50e5543747b09303498f0f6acef13
mproncace <mproncace@gmail.com>
2013-12-07 11:40:52 -0500

    Screw it, I'm done with Maven

---
+++ TenDotJava/src/net/amigocraft/tendotjava/TenDotJava.java
@@ -0,0 +1,15 @@
+package net.amigocraft.tendotjava;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TenDotJava extends JavaPlugin {
+
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+	
+}
